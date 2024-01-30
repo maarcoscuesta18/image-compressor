55f9195e87c5fe33bbb09ff2291682222cbc092b
maarcoscuesta18 <marcosyjaime2@gmail.com>
2024-01-30 19:13:59 +0100

    first commit, first version compressor

---
+++ src/components/content.js
@@ -0,0 +1,70 @@
+export const instructions = {
+    "instructions": {
+      "en": "Instructions",
+      "es": "Instrucciones",
+      "pt-br": "Instruções"
+    },
+    "en": [
+      "Select or drag and drop your image file into the designated area.",
+      "Wait for the image to be automatically compressed.",
+      "View the original and new sizes of the compressed image.",
+      "Download the compressed image by clicking the download link."
+    ],
+    "es": [
+      "Selecciona o arrastra y suelta tu archivo de imagen en el área designada.",
+      "Espera a que la imagen se comprima automáticamente.",
+      "Visualiza los tamaños original y nuevo de la imagen comprimida.",
+      "Descarga la imagen comprimida haciendo clic en el enlace de descarga."
+    ],
+    "pt-br": [
+      "Selecione ou arraste e solte seu arquivo de imagem na área designada.",
+      "Aguarde a imagem ser comprimida automaticamente.",
+      "Veja os tamanhos original e novo da imagem comprimida.",
+      "Baixe a imagem comprimida clicando no link de download."
+    ]
+  };
+
+export const footer = {
+  "en": "© 2024 Image compressor made with ❤️ by Marcos Cuesta. All rights reserved.",
+  "es": "© 2024 Compresor de imágenes hecho con ❤️ por Marcos Cuesta. Todos los derechos reservados.",
+  "pt-br": "© 2024 Compressor de imagens feito com ❤️ por Marcos Cuesta. Todos os direitos reservados."
+};
+
+export const compressor = {
+  "en": {
+    "title": "Image compressor",
+    "description": "Compress your images for free, maintaining the quality of the image.",
+    "button": "Compress image",
+    "compressing": "Compressing...",
+    "drag": "Drag & Drop your image",
+    "or": "or",
+    "original_size": "Original size: ",
+    "new_size": "New size: ",
+    "download": "Download your compressed file!",
+    "success": "Compression successful!"
+  },
+  "es": {
+    "title": "Compresor de imágenes",
+    "description": "Comprime tus imágenes de forma gratuita, manteniendo la calidad de la imagen.",
+    "button": "Comprimir imagen",
+    "compressing": "Comprimiendo...",
+    "drag": "Arrastra y suelta tu imagen",
+    "or": "o",
+    "original_size": "Tamaño original: ",
+    "new_size": "Nuevo tamaño: ",
+    "download": "¡Descarga tu archivo comprimido!",
+    "success": "¡Compresión exitosa!"
+  },
+  "pt-br": {
+    "title": "Compressor de imagens",
+    "description": "Comprima suas imagens gratuitamente, mantendo a qualidade da imagem.",
+    "button": "Comprimir imagem",
+    "compressing": "Comprimindo...",
+    "drag": "Arraste e solte sua imagem ",
+    "or": "ou",
+    "original_size": "Tamanho original: ",
+    "new_size": "Novo tamanho: ",
+    "download": "Baixe seu arquivo comprimido!",
+    "success": "Compressão bem sucedida!"
+  }
+};
---
+++ src/components/serviceWorker.js
@@ -0,0 +1,58 @@
+function readableBytes(bytes) {
+  const i = Math.floor(Math.log(bytes) / Math.log(1024)),
+      sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
+}
+
+let image = document.querySelector('#image');
+image.addEventListener('change', async function() {
+  // GET IMAGE
+  const imageFile = this.files[0];
+
+  // SET OPTIONS
+  const options = {
+      maxSizeMB: .3,
+      maxWidthOrHeight: 1000,
+      useWebWorker: true
+  }
+
+  document.getElementById('loading').classList.remove('hidden');
+  document.querySelector('#compression-success').textContent = '';
+  document.querySelector('#compression-download').textContent = '';
+  document.querySelector('#orig-size').textContent = '';
+  document.querySelector('#new-size').textContent = '';
+
+  try {
+      const compressedFile = await imageCompression(imageFile, options);
+
+      // DISPLAY FILE SIZES
+      document.querySelector('#orig-size').textContent = readableBytes(imageFile.size);
+      document.querySelector('#new-size').textContent = readableBytes(compressedFile.size);
+      document.querySelector('#compression-success').textContent = 'Compression successful!';
+      document.querySelector('#compression-download').textContent = 'Download your compressed file!';
+
+      // SET OUTPUT IMAGE
+      let output = document.querySelector('#output');
+      var img = document.getElementById('output');
+      img.classList.remove('hidden'); 
+    
+      output.src = URL.createObjectURL(compressedFile);
+
+      // SHOW HIDDEN ELEMENTS
+      document.getElementById('loading').classList.add('hidden');
+
+      let infoContainer = document.querySelector('#info-container');
+      infoContainer.style.display = 'flex';
+
+      let dlContainer = document.querySelector('#dl-container');
+      dlContainer.style.display = 'flex';
+
+      // SET SOURCE IN DL BUTTON
+      let dl = document.querySelector('#download-link');
+      dl.setAttribute('href', output.src);
+      
+  } catch(error) {
+      console.log(error);
+      document.getElementById('loading').classList.add('hidden');
+  }
+});
